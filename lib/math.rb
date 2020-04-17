f3c5041e7cb7673a14bc6da5f5671cf0eeaf2532
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 03:27:51 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  sum = num2 + num1
+  sum
 end
 
 def subtraction(num1, num2)
+  result = num1 - num2
+  result
 
 end
 
 def division(num1, num2)
+  
+   num1 / num2
+  
+  
 
 end
 
 def multiplication(num1, num2)
-
+  result = num1 ** num2 
+  result 
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num 2 
+  result
 end
 
 def square_root(num)
+  result = math.sqrt(num)
+  result 
 
 end
